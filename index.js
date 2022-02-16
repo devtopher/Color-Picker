2f830d9acba0ca43e379c0def5fcef2e13db8347
devtopher <christopher.david.mccoy@gmail.com>
2022-02-16 13:26:38 -0500

    added functionality and made it work

---
+++ index.js
@@ -0,0 +1,77 @@
+
+
+let schemeEl= document.getElementById("color-scheme")
+let colorEl = document.getElementById("color-picker")
+let chosenScheme = schemeEl.value
+let chosenColor = colorEl.value
+chosenColor = chosenColor.substring(1)
+
+let hexOne = document.getElementById("hex-one")
+let hexTwo = document.getElementById("hex-two")
+let hexThree = document.getElementById("hex-three")
+let hexFour = document.getElementById("hex-four")
+let hexFive = document.getElementById("hex-five")
+let hexArr = [hexOne, hexTwo, hexThree, hexFour, hexFive]
+console.log(hexArr)
+
+let colorOne = document.getElementById("color-one")
+let colorTwo = document.getElementById("color-two")
+let colorThree = document.getElementById("color-three")
+let colorFour = document.getElementById("color-four")
+let colorFive = document.getElementById("color-five")
+let colorElArr = [colorOne, colorTwo, colorThree, colorFour, colorFive]
+
+let colorsArr = []
+
+
+
+
+
+
+function createUrl(){
+    url = new URL("https://www.thecolorapi.com/scheme?format=json")
+    let params = new URLSearchParams(url.search)
+    chosenColor = colorEl.value
+    chosenScheme = schemeEl.value
+    params.append("hex",chosenColor)
+    params.append("mode",chosenScheme)
+    params.append("count",5)
+    //console.log(params.toString())
+    url.search = params.toString()
+    //console.log(url.toString())
+    return url
+}
+
+
+document.getElementById("selection-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    let url = createUrl()
+    //console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        for(let i=0; i<data.colors.length;i++){
+            
+            colorsArr.push(data.colors[i].hex.value)
+        }
+        for(let i=0; i<hexArr.length; i++){
+            hexArr[i].textContent = colorsArr[i]
+            colorElArr[i].style.background = colorsArr[i] 
+        }
+        
+
+    })
+   
+    
+    
+})
+
+// let url = `https://www.thecolorapi.com/id?hex=${chosenColor}&mode=${chosenScheme}&count=5`
+// colorEl.addEventListener("input",()=>{
+//     chosenColor = colorEl.value
+//     console.log(chosenColor)
+// })
+// schemeEl.addEventListener("input",()=>{
+//     chosenScheme = schemeEl.value
+//     console.log(chosenScheme)
+// })
